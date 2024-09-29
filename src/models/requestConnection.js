df9610b1d2720eb021ef127f1b6010c238ca82ec
virendra jangid <vrajjangid5@gmail.com>
2024-09-29 23:38:36 +0530

    user route

--- src/models/requestConnection.js
+++ src/models/requestConnection.js
@@ -9,6 +9,7 @@ const connectionRequestSchema= new mongoose.Schema({
     },
     toUserId:{
         type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
         required:true,
     },
     status:{
--- src/routes/user.js
+++ src/routes/user.js
@@ -25,5 +25,33 @@ userRouter.get("/user/requests/received",userAuth,async(req,res)=>{
     }
 })
 
+userRouter.get("/user/connections",userAuth,async(req,res)=>{
+    try {
+        const loggedInUser= req.user;
+        const connectionReq = await connectionRequest.find({
+            $or:[
+                {toUserId:loggedInUser._id,status:"accepted"},
+                {fromUserId:loggedInUser._id,status:"accepted"}
+            ]
+        }).populate("fromUserId","firstName lastName").populate("toUserId","firstName lastName");
+        const data = connectionReq.map((row)=>{
+            if(row.fromUserId._id.toString()==loggedInUser._id.toString()){
+                return row.toUserId;
+            }
+            return row.fromUserId;
+        })
+        res.send({data});
+    } catch (error) {
+        res.status(400).send("ERROR : "+error.message);
+    }
+})
+
+userRouter.get("/user/feed",userAuth,async(req,res)=>{
+    try {
+        
+    } catch (error) {
+        res.status(400).send("Error : "+error.message);
+    }
+})
 
 module.exports= userRouter;
