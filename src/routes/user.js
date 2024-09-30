0ceb26732119cd2b39332bea222871d04a16c0af
virendra jangid <vrajjangid5@gmail.com>
2024-09-30 08:22:36 +0530

    user feed route

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const userRouter = express.Router();
+const User = require("../models/user");
 
 const {userAuth}= require("../middlewares/auth");
 
@@ -48,6 +49,31 @@ userRouter.get("/user/connections",userAuth,async(req,res)=>{
 
 userRouter.get("/user/feed",userAuth,async(req,res)=>{
     try {
+        const loggedInUser= req.user;
+
+        const page = parseInt(req.query.page)||1;
+        let limit=parseInt(req.query.limit)||10;
+        limit=limit>50?50:limit;
+        const skip= (page-1)*limit;
+
+        const connectionReq = await connectionRequest.find({
+            $or:[
+                {fromUserId:loggedInUser},
+                {toUserId:loggedInUser},
+            ]
+        }).select("fromUserId toUserId");
+        const hideUserFromFeed = new Set();
+        connectionReq.forEach((req)=>{
+            hideUserFromFeed.add(req.fromUserId.toString()),
+            hideUserFromFeed.add(req.toUserId.toString())
+        })
+        const users= await User.find({
+            $and:[
+                {_id:{$nin:Array.from(hideUserFromFeed)}},
+                {_id:{$ne:loggedInUser._id}},
+            ]
+        }).select("firstName lastName").skip(skip).limit(limit);
+        res.send(users);
         
     } catch (error) {
         res.status(400).send("Error : "+error.message);
